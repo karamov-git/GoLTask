482329cf7595ed0d014291d6596686261ae2c4bd
Карамов Артур <32008462+karamov-git@users.noreply.github.com>
2022-12-02 13:58:30 +0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameOfLive
+{
+	public static class Game
+	{
+		private static Random _random = new Random();
+
+		public static bool[,] GetNextState(bool[,] currentState)
+		{
+			currentState[_random.Next(0, 100), _random.Next(0, 100)] = true;
+			return currentState;
+		}
+
+		public static bool[,] GetInitialState()
+		{
+			var result = new bool[100, 100];
+			result[50, 50] = result[51, 50] = result[52, 50] = true;
+			return result;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace GameOfLive
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			[DllImport("kernel32.dll", SetLastError = true)]
+			static extern IntPtr GetConsoleWindow();
+
+			try
+			{
+				var gameState = Game.GetInitialState();
+				var width = gameState.GetLength(0);
+				var height = gameState.GetLength(1);
+				using var gfx = Graphics.FromHwnd(GetConsoleWindow());
+				var coefW = gfx.VisibleClipBounds.Width / width;
+				var coefH = gfx.VisibleClipBounds.Height / height;
+				using Pen pen = new(new SolidBrush(Color.Green));
+				gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+				while (true)
+				{
+					gfx.Clear(Color.Black);
+					for (var i = 0; i < width; i++)
+					{
+						for (var j = 0; j < height; j++)
+						{
+							if (!gameState[i, j]) continue;
+							
+							var rectangle = new Rectangle(i * (int)coefW + 10, j * (int)coefH + 10, 9, 9);
+							gfx.FillRectangle(Brushes.Green, rectangle);
+						}
+					}
+
+					Thread.Sleep(1000);
+
+					gameState = Game.GetNextState(gameState);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
+	}
+}
